085ff3cbe64ebd09dabe532c7e2fde9e26259750
larrybuntus <larrybuntus@gmail.com>
2018-12-04 07:41:03 +0000

    added routes fot web and api

---
+++ app/routes/web.php
@@ -0,0 +1,58 @@
+<?php 
+    namespace App\Routes;
+
+    use App\Controllers as Controller;
+
+    class Web {
+        public function __construct()
+        {
+            
+        }
+
+        public static function get($url, $path) {
+            // call setter
+            $currentController = new Controller\App();
+            $currentMethod     = index;
+            $params            = [];
+            
+            $start = $url[0];
+            unset($url[0]);
+
+            switch ($start) {
+                default:
+                    list($currentController, $currentMethod, $url) = [$currentController, $currentMethod, $url];
+                    break;                
+            }
+            
+            $params = $url ? array_values($url) : [];
+
+            call_user_func_array([$currentController, $currentMethod], $params);
+        }
+
+        public static function post($url, $path) {
+            $currentController = new Controller\App();
+            $currentMethod     = index;
+            $params            = [];
+
+            $start = $url[0];
+            unset($url[0]);
+
+            switch ($start) {
+                default:
+                    list($currentController, $currentMethod, $url) = [$currentController, $currentMethod, $url];
+                    break;
+            }
+
+            $params = $url ? array_values($url) : [];
+            call_user_func_array([$currentController, $currentMethod], $params);
+        }
+
+        public static function put($url, $path) {
+
+        }
+
+        public static function delete($url, $path) {
+
+        }
+    }
+?>
