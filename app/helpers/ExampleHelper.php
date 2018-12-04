2ef79f583b6ce03f5a220c0e8c35ad4c3f75d245
larrybuntus <larrybuntus@gmail.com>
2018-12-04 07:39:44 +0000

    added helpers for controllers and models

---
+++ app/helpers/ExampleHelper.php
@@ -0,0 +1,18 @@
+<?php 
+namespace App\Helpers;
+
+use App\Lib\Base as Base;
+
+class Example extends Base
+{
+
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        
+    }
+}
+?>
