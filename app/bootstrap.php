826d0dde871824a077001e57025dc83103eade27
larrybuntus <larrybuntus@gmail.com>
2018-12-04 07:42:00 +0000

    addiion of routes and proper use of namespace

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,8 +1,12 @@
-<?php 
+<?php
     // load config
     require_once 'config/config.php';
+    // require_once 'lib/vendor/autoload.php';
+    require_once 'routes/web.php';
+    require_once 'routes/api.php';
 
     // autoload core libraries
-    spl_autoload_register(function($className){
-        require_once "lib/{$className}.php";
+    spl_autoload_register(function ($className) {
+        $part = explode('\\', $className);
+        require 'lib/' . end($part). '.php';
     });
