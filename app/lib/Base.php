3497124f927db102b8b534778aad4b034bfeab6b
larrybuntus <larrybuntus@gmail.com>
2018-12-04 07:39:18 +0000

    namespaced base and additional methods added

--- app/lib/Base.php
+++ app/lib/Base.php
@@ -1,86 +1,120 @@
-<?php 
+<?php
     /*
     * this is the base controller
     * this loads the model and views
     */
+namespace App\Lib;
 
-    class Base {
-    /*
-    *load model and view
-    */ 
+use App\Models as Model;
+class Base
+{
 
-        public function model($model){
-            // require model file
-            require_once dirname(__dir__)."/models/{$model}.php";
+    public function __construct()
+    {
+        
+    }
 
-            return new $model();
-        }
+    public function model($model)
+    {
+        $parts = explode("\\", $model);
+        $m     = end($parts);
 
-        public function view($view, $data = [] ){
-            if (file_exists(dirname(__dir__)."/views/{$view}.php")){
-                require_once dirname(__dir__)."/views/{$view}.php";
-            }else{
-                require_once dirname(dirname(__dir__))."/public/html/404.html";
-            }
-        }
+        // require model file
+        require_once dirname(__dir__)."/models/{$m}.php";
+        return new $model();
+    }
 
-        public function img_upload($path,$image){
-            // get image details
-            $name = strtotime(date("Y-m-d H:i:s")).'_'.$image["name"];
-            $temp = $image["tmp_name"];
-            $size = $image["size"];
+    public function view($view, $data = [])
+    {
+        if (file_exists(dirname(__dir__)."/views/{$view}.php"))
+            include_once dirname(__dir__)."/views/{$view}.php";
+        else
+            include_once dirname(dirname(__dir__))."/public/html/404.html";
+    }
 
-            $target_dir         =   $path;                                                  // destination path
-            $target_file        =   $target_dir . basename($name);                    // destination file
-            $upload_ok          =   1;                                                      // upload checker
-            $image_file_type    =   strtolower(pathinfo($target_file,PATHINFO_EXTENSION));  // file type
+    public function img_upload($path, $image)
+    {
+        // get image details
+        $name = strtolower(strtotime(date("Y-m-d H:i:s")).'_'.$image["name"]);
+        $temp = $image["tmp_name"];
+        $size = $image["size"];
+
+        $target_dir         =   $path;                                                  // destination path
+        $target_file        =   $target_dir . basename($name);                    // destination file
+        $upload_ok          =   1;                                                      // upload checker
+        $image_file_type    =   strtolower(pathinfo($target_file, PATHINFO_EXTENSION));  // file type
             
-            // Check if file already exists
-            if (file_exists($target_file)) {
-                $upload_ok = 0;
-                return [true, null];
-            }
-            // Check file size
-            if ($size > 10000000) {
-                $upload_ok = 0;
-                return [false, "Image too big"];
-            }
-            // Allow certain file formats
-            if($image_file_type != "jpg" && $image_file_type != "png" && $image_file_type != "jpeg"
-            && $image_file_type != "gif" ) {
-                $upload_ok = 0;
-                return [false, $name." format not acceptable"];
-            }
-            // Check if $upload_ok is set to 0 by an error
-            if ($upload_ok == 0) 
-                // return error
-                return [false, $name.": An error occured!"];
-            else 
-                if (move_uploaded_file($temp, $target_file))
-                    return [true, $name];
-                else
-                    return [false, "Wasn't able to upload {$name}"];
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            $upload_ok = 0;
+            return [true, $name];
         }
-
-        public function sendmail($emails, $subject, $message){
-
-            // headers
-            $headers    =   "From: " . strip_tags("no-reply@email.com") . "\r\n";
-            $headers    .=  "MIME-Version: 1.0\r\n";
-            $headers    .=  "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-            if (is_array($emails)):
-                foreach ($emails as $email)
-                    if (isset($email) && !empty($email)):
-                        mail($email, $subject, $this->email_template($message), $headers);
-                    endif;
-            else:
-                mail($emails, $subject, $this->email_template($message), $headers);
-            endif;
+        // Check file size
+        if ($size > 10000000) {
+            $upload_ok = 0;
+            return [false, "Image too big"];
         }
-
-        public function email_template($message){
-            return $message;
+        // Allow certain file formats
+        if ($image_file_type != "jpg" && $image_file_type != "png" && $image_file_type != "jpeg"
+            && $image_file_type != "gif"
+        ) {
+            $upload_ok = 0;
+            return [false, $name." format not acceptable"];
+        }
+        // Check if $upload_ok is set to 0 by an error
+        if ($upload_ok == 0) {
+            // return error
+            return [false, $name.": An error occured!"];
+        } elseif (move_uploaded_file($temp, $target_file)) {
+            return [true, $name];
+        } else {
+                return [false, "Wasn't able to upload {$name}"];
         }
     }
-?>
+
+    public function sendmail($emails, $subject, $message, $view)
+    {
+
+        // headers
+        $headers    =   "From: " . strip_tags("no-reply@klikgh.com") . "\r\n";
+        $headers    .=  "MIME-Version: 1.0\r\n";
+        $headers    .=  "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+        if (is_array($emails)) :
+            foreach ($emails as $email) {
+                if (isset($email) && !empty($email)) :
+                    mail($email, $subject, $this->email_template($message, $view), $headers);
+                endif;
+            };
+        else :
+                mail($emails, $subject, $this->email_template($message, $view), $headers);
+        endif;
+    }
+
+    public function email_template($message, $view)
+    {
+        ob_start();
+        include dirname(__dir__)."/views/mailers/{$view}.html.php";
+        $template = ob_get_clean();
+        return $template;
+    }
+
+
+    function startsWith($haystack, $needle)
+    {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+
+    public function objectify($array){
+        return \json_decode(\json_encode($array));
+    }
+
+    public function return($code, $return)
+    {
+        http_response_code($code);
+        echo json_encode($return);
+
+        exit();
+    }
+}
