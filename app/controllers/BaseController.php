ac1963ff12ada91b6ccc6bec2d67538469f207b5
lawrence.adu <lawrence.adu@nfortics.com>
2019-02-18 11:30:42 +0000

    base controller for all controllers to inherit from

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Lib\Base as BaseLib;
+
+// use App\Helpers\Example;
+class Base extends BaseLib
+{
+    public function __construct()
+    {
+
+    }
+}
