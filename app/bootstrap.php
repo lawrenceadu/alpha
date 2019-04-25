b0c0660e9ae05dec08ba1df32c91839982ecee3c
larry buntus <larrybuntus@gmail.com>
2019-04-25 13:36:16 +0000

    removed spl_autoload_register

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,6 @@
 <?php
-    // load config
     require_once 'config/config.php';
     require_once 'lib/vendor/autoload.php';
+
     require_once 'routes/web.php';
     require_once 'routes/api.php';
-
-    // autoload core libraries
-    spl_autoload_register(function ($className) {
-        $part = explode('\\', $className);
-        require 'lib/' . end($part). '.php';
-    });
