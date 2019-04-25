f610d73382c1cb1dc326bc2e870566b22145242c
larry buntus <larrybuntus@gmail.com>
2019-04-25 13:35:13 +0000

    removed unnecessary require code

--- app/lib/Core.php
+++ app/lib/Core.php
@@ -7,10 +7,6 @@
 
 namespace App\Lib;
 
-use RecursiveDirectoryIterator as RecursiveDirectory;
-use RecursiveIteratorIterator as RecursiveIterator;
-use RegexIterator as Regex;
-use RecursiveRegexIterator as RecursiveRegex;
 use App\Routes as Routes;
 class Core
 {
@@ -19,36 +15,13 @@ class Core
         // assign get url to url variable
         list($url, $path)  = $this->getUrl();
 
-        // Recursive Directory Iterator to get all Controller files
-        $Directory = new RecursiveDirectory(dirname(__DIR__) . '/controllers');
-        $Iterator  = new RecursiveIterator($Directory);
-        $Regex     = new Regex($Iterator, '/^.+\.php$/i', RecursiveRegex::GET_MATCH);
-
         if($url[0] == "api")
-            $Routes = new Routes\Api($url, $path);
+            $route = new Routes\Api($url, $path);
         else    
-            $Routes = new Routes\Web($url, $path);
+            $route = new Routes\Web($url, $path);
 
-        // Get all the helpers
-        foreach (glob(dirname(__dir__) . '/helpers/*.php') as $key => $helpers)
-            require_once $helpers;
 
-        // Look in controllers for controller
-        $paths = [];
-        foreach ($Regex as $path_to_file)
-            $paths[] = $path_to_file[0];
-
-        sort($paths);
-
-        // require controllers
-
-        require_once __APPROOT__ . '/controllers/BaseController.php';
-
-        foreach($paths as $path)
-            if(strpos($path, 'BaseController') === false)
-                require_once $path;
-
-        $Routes->request();
+        $route->request();
     }
 
     public function getUrl()
