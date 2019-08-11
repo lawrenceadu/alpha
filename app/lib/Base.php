228f5374cfbab0fe2bae96e8bf8674939bc74ef4
larrybuntus <larrybuntus@gmail.com>
2019-08-11 02:17:26 +0000

    removed all unused functions

--- app/lib/Base.php
+++ app/lib/Base.php
@@ -8,29 +8,22 @@ namespace App\Lib;
 use App\Models as Model;
 class Base
 {
+    public $twig;
 
     public function __construct()
     {
-        
-    }
-
-    public function model($model)
-    {
-        $parts = explode("\\", $model);
-        $m     = end($parts);
-
-        // require model file
-        require_once dirname(__dir__)."/models/{$m}.php";
-        return new $model();
+        $loader = new \Twig\Loader\FilesystemLoader(__ROOT__ . '/app/views');
+        $this->twig = new \Twig\Environment($loader);
     }
 
     public function view($view, $data = [])
     {
-        extract($data, EXTR_PREFIX_SAME, "wddx");
-        if (file_exists(dirname(__dir__)."/views/{$view}.php"))
-            include_once dirname(__dir__)."/views/{$view}.php";
-        else
-            include_once dirname(dirname(__dir__))."/public/html/404.html";
+        error_log($view);
+        if (file_exists(dirname(__dir__)."/views/{$view}.twig")):
+            echo $this->twig->render($view . '.twig', $data);
+        else:
+            require_once dirname(dirname(__dir__))."/public/html/404.html";
+        endif;
     }
 
     public function file_upload($path, $file, $file_category = "image"): Array
@@ -45,8 +38,6 @@ class Base
         $upload_ok      =   true;                                                      // upload checker
         $file_type      =   strtolower(pathinfo($target_file, PATHINFO_EXTENSION));  // file type
 
-
-        echo $target_file;
             
         if (!file_exists($path)):
             mkdir($path, 0777, true);
@@ -82,34 +73,6 @@ class Base
         }
     }
 
-    public function sendmail($emails, $subject, $message, $view)
-    {
-
-        // headers
-        $headers    =   "From: " . strip_tags("no-reply@klikgh.com") . "\r\n";
-        $headers    .=  "MIME-Version: 1.0\r\n";
-        $headers    .=  "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-        if (is_array($emails)) :
-            foreach ($emails as $email) {
-                if (isset($email) && !empty($email)) :
-                    mail($email, $subject, $this->email_template($message, $view), $headers);
-                endif;
-            };
-        else :
-                mail($emails, $subject, $this->email_template($message, $view), $headers);
-        endif;
-    }
-
-    public function email_template($message, $view)
-    {
-        ob_start();
-        include dirname(__dir__)."/views/mailers/{$view}.html.php";
-        $template = ob_get_clean();
-        return $template;
-    }
-
-
     function startsWith($haystack, $needle): Bool
     {
         $length = strlen($needle);
@@ -119,19 +82,6 @@ class Base
     public function objectify($array): Object
     {
         return \json_decode(\json_encode($array));
-    }
-
-    public function return($code, $return)
-    {
-        http_response_code($code);
-        echo json_encode($return);
-
-        exit();
-    }
-    
-    public function image_path($path_to_image): String
-    {
-        return __URLROOT__.'/public/assets/img/' . $path_to_image;
     }
 
     public function permit($array, $permitables): Array
