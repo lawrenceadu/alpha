f9ddcccd978539053c1cc3f35f946add1d3baffd
larrybuntus <larrybuntus@gmail.com>
2019-10-27 21:16:32 +0000

    update to bootstrap and deleted lib directory

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'config/config.php';
-    require_once __ROOT__ . '/vendor/autoload.php';
+    require_once dirname(__DIR__).'/vendor/autoload.php';
 
     require_once 'routes/web.php';
     require_once 'routes/api.php';
