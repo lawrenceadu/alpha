d51af79205e5dcbc3eac79968014f9cd9ab03d87
larrybuntus <larrybuntus@gmail.com>
2019-12-06 10:53:23 +0000

    removed config and added dotenv

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
-    require_once 'config/config.php';
     require_once dirname(__DIR__).'/vendor/autoload.php';
+
+    // start env
+    \Dotenv\Dotenv::create(dirname(__DIR__))->load();
 
     require_once 'routes/web.php';
     require_once 'routes/api.php';
