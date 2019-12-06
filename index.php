7506da27aca1166ad11aa7514ab003a626d11682
larrybuntus <larrybuntus@gmail.com>
2019-12-06 10:53:04 +0000

    removed unnecessary  and core

--- index.php
+++ index.php
@@ -6,12 +6,5 @@
     // start session
     session_start();
 
-    // get request url
-    $_GET["url"] = $_SERVER['REQUEST_URI'];
-
     // require the bootstrap file
     require_once __DIR__."/app/bootstrap.php";
-
-    use App\Lib\Core;
-    // init core lib
-    new Core();
